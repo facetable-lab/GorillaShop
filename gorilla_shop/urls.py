6aec72775d7318653393e31a0d388e0235661c25
facetable-lab <diofff6@gmail.com>
2023-05-14 02:33:04 +0300

    Маршрутизация страниц

--- gorilla_shop/urls.py
+++ gorilla_shop/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
+from products import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', views.home, name='home'),
+    path('products/', views.products, name='products'),
 ]
